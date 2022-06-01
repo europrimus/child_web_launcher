dfa9ec1ece28c6aa62aa03c99d4ae7b5b80c73cf
Europrimus <dev@europrimus.ninja>
2022-06-01 14:56:09 +0200

    display Links from local storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	export let name: string;
+	import Links from './Links.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Links />
 </main>
 
 <style>
---
+++ src/Links.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { writable, get } from "svelte/store";
+
+	const storedLinks = writable(
+		JSON.parse(
+			localStorage.getItem("links" || "[]")
+		)
+	);
+	storedLinks.subscribe(value => {
+		localStorage.setItem("links", JSON.stringify(value));
+	});
+</script>
+
+<section>
+	{#if Array.isArray(get(storedLinks))}
+		{#each get(storedLinks) as link}
+			<article>
+				<a href="{link.url}"><img src="{link.img}" /></a>
+			</article>
+		{/each}
+	{/if}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+	}
+	article {
+		text-align: center;
+		padding: 1em;
+		max-width: 5em;
+		margin: 0 auto;
+	}
+	a {
+		display: block;
+		width: 100%;
+		height: 100%;
+		aspect-ratio: 1 / 1;
+		background: #fff;
+		padding: 2em;
+		border-radius: 1em;
+	}
+	img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+
+</style>
